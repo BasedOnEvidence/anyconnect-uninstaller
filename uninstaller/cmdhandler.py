7290500db432866757b861fb7ecf48d82788f156
BasedOnEvidence <TestTypoTest@gmail.com>
2021-07-20 09:16:43 +0300

    Fix encoding error

--- uninstaller/cmdhandler.py
+++ uninstaller/cmdhandler.py
@@ -7,7 +7,7 @@ logger = get_logger(__name__)
 
 def make_list_from_cmd_output(command, splitter=None):
     try:
-        out_str = subprocess.check_output(command).decode("utf-8")
+        out_str = subprocess.check_output(command).decode("cp1252")
         result_list = out_str.split(splitter)
     except (subprocess.CalledProcessError, WindowsError) as err:
         result_list = []
